74e1ac0ca285aa963de1079fdd81fead25771035
dewyn <dewyn.liriano@gmail.com>
2018-12-11 13:38:48 -0400

    Se crean las entradas de nuevos equipos, los equipos sin seriales se crean de manera automática, los que requieren seriales piden los seriales de la cantidad de equipos que se digitó.

--- grails-app/controllers/gestion_equipos_controlados/MovimientoController.groovy
+++ grails-app/controllers/gestion_equipos_controlados/MovimientoController.groovy
@@ -12,31 +12,54 @@ class MovimientoController {
 
     def crearEntrada() {
         def listadoEquipos = Equipo.findAllByHabilitado(true)
-        [listadoEquipos : listadoEquipos]
+        [listadoEquipos: listadoEquipos]
     }
 
     def crearSalida() {
         def listadoEquipos = Equipo.findAllByHabilitado(true)
-        [listadoEquipos : listadoEquipos]
+        [listadoEquipos: listadoEquipos]
     }
 
-    def guardarEntrada(Movimiento movimiento){
+    def guardarEntrada() {
+        println params
+        def equipo = Equipo.findById(params.equipo as long)
+        def movimiento = new Movimiento()
+
+        if(equipo != null){
+            movimiento.equipo = equipo
+            movimiento.cantidad = params.cantidad as int
+            movimiento.tipoMovimiento = Movimiento.TipoMovimiento.ENTRADA
+            if(params.serial != null){
+                params.serial.each {
+                    new EquipoSerial(equipo: equipo, serial: it).save(flush:true, failOnError: true)
+                }
+            }
+        }
+
         try {
-            movimiento.save(flush:true, failOnError:true)
+            movimiento.save(flush: true, failOnError: true)
             redirect(controller: 'movimiento', action: 'index')
         }
-        catch (Exception e){
+        catch (Exception e) {
             respond movimiento.errors, view: 'crearEntrada'
         }
     }
 
-    def guardarSalida(Movimiento movimiento){
+    def guardarSalida(Movimiento movimiento) {
         try {
-            movimiento.save(flush:true, failOnError:true)
+            movimiento.save(flush: true, failOnError: true)
             redirect(controller: 'movimiento', action: 'index')
         }
-        catch (Exception e){
+        catch (Exception e) {
             respond movimiento.errors, view: 'crearEntrada'
         }
+    }
+
+    def verificarNecesitaSerial() {
+        def equipo = Equipo.findById(params.data as long)
+        if (equipo != null)
+            render equipo.serial
+        else
+            render 'null'
     }
 }
