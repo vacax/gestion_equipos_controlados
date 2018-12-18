c19dcf4395e9b5fd225cf364b3abb0016ed069e2
dewyn <dewyn.liriano@gmail.com>
2018-12-18 15:20:20 -0400

    Creando controller para préstamos

---
+++ src/test/groovy/gestion_equipos_controlados/PrestamoControllerSpec.groovy
@@ -0,0 +1,18 @@
+package gestion_equipos_controlados
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class PrestamoControllerSpec extends Specification implements ControllerUnitTest<PrestamoController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
