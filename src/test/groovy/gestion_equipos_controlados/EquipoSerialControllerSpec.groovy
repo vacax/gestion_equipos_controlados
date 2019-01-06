e3087c6815bef99b7bba84bdecdc3b0caef072ad
dewyn <dewyn.liriano@gmail.com>
2019-01-05 21:03:30 -0400

    .s

---
+++ src/test/groovy/gestion_equipos_controlados/EquipoSerialControllerSpec.groovy
@@ -0,0 +1,18 @@
+package gestion_equipos_controlados
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class EquipoSerialControllerSpec extends Specification implements ControllerUnitTest<EquipoSerialController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/gestion_equipos_controlados/UsuarioControllerSpec.groovy
@@ -0,0 +1,18 @@
+package gestion_equipos_controlados
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class UsuarioControllerSpec extends Specification implements ControllerUnitTest<UsuarioController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
